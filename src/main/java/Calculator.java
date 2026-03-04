fb4f6e506af639aa2fd9ecb6fcf727318fe590c7
Nikolaos Moraru <134730407+klauseen@users.noreply.github.com>
2026-03-04 17:35:34 +0200

    Complete calculator UI and logic implementation with error handling

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,92 +1,38 @@
+package main.java;
+
+
+
 
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
-	public static void main(String[] args) {
-		boolean statement;
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter the first number");
-		double number1 = 0;
 
-		do {
-			try {
-				statement = false;
-				number1 = scanner.nextDouble();
-			} catch (InputMismatchException e) {
-				statement = true;
-				System.out.println("Please enter a number");
-				scanner.next();
-			}
-		} while (statement);
 
-		System.out.println("Enter the operation");
-		char operation;
-
-		do {
-			operation = scanner.next().charAt(0);
-			if (operation != '/' && operation != '*' && operation != '-' && operation != '+') {
-				statement = true;
-				System.out.println("Please enter an operation");
-			} else {
-				statement = false;
-			}
-
-		} while (statement);
-
-		System.out.println("Enter the second number");
-		double number2 = 0;
-
-		do {
-			try {
-				statement = false;
-				number2 = scanner.nextDouble();
-			} catch (InputMismatchException e) {
-				statement = true;
-				System.out.println("Enter a number");
-				scanner.next();
-			}
-		} while (statement);
-
-		switch (operation) {
-		case '/':
-			division(number1, number2);
-			return;
-		case '*':
-			multiplication(number1, number2);
-			return;
-		case '-':
-			subtraction(number1, number2);
-			return;
-		case '+':
-			addition(number1, number2);
-			return;
-		}
+	public static double addition(double number1, double number2) {
+		return number1 + number2;
 	}
 
-	public static void addition(double number1, double number2) {
-		double addition = number1 + number2;
-		System.out.println(addition);
-	}
-
-	public static void division(double number1, double number2) {
+	public static double division(double number1, double number2) {
 		if(number2 == 0) {
-			System.out.println("Can't make division to 0");
+			return 0;
 		}else {
-		double division = number1 / number2;
-		System.out.println(division);
+		return number1 / number2;
 		}
 	}
 
-	public static void subtraction(double number1, double number2) {
-		double substraction = number1 - number2;
-		System.out.println(substraction);
+	public static double subtraction(double number1, double number2) {
+		return number1 - number2;
+
 	}
 
-	public static void multiplication(double number1, double number2) {
-		double multiplication = number1 * number2;
-		System.out.println(multiplication);
+	public static double multiplication(double number1, double number2) {
+		return number1 * number2;
+	}
+
+	public static double toPercentage(double number) {
+		return number / 100;
 	}
 
 }
---
+++ src/main/java/CalculatorUI.java
@@ -0,0 +1,250 @@
+package main.java;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Font;
+
+public class CalculatorUI extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private static JTextField display;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CalculatorUI frame = new CalculatorUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public CalculatorUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 330, 490);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		display = new JTextField();
+		display.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		display.setBounds(10, 34, 296, 73);
+		contentPane.add(display);
+		display.setColumns(10);
+		
+		display.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    Result();
+                }
+            }
+        });
+		
+		ActionListener commonListener = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String command = e.getActionCommand();
+				
+				processButtons(command);
+			}
+		};
+		
+		JButton btnDel = new JButton("DEL");
+		btnDel.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		btnDel.setBounds(20, 159, 49, 29);
+		contentPane.add(btnDel);
+		btnDel.addActionListener(commonListener);
+		
+		JButton btnAC = new JButton("AC");
+		btnAC.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		btnAC.setBounds(92, 159, 49, 29);
+		contentPane.add(btnAC);
+		btnAC.addActionListener(commonListener);
+		
+		JButton btnPercentage = new JButton("%");
+		btnPercentage.setBounds(166, 159, 49, 29);
+		contentPane.add(btnPercentage);
+		btnPercentage.addActionListener(commonListener);
+		
+		JButton btnDivision = new JButton("/");
+		btnDivision.setBounds(240, 159, 49, 29);
+		contentPane.add(btnDivision);
+		btnDivision.addActionListener(commonListener);
+		
+		JButton btnSeven = new JButton("7");
+		btnSeven.setBounds(20, 199, 49, 29);
+		contentPane.add(btnSeven);
+		btnSeven.addActionListener(commonListener);
+		
+		JButton btnEight = new JButton("8");
+		btnEight.setBounds(92, 199, 49, 29);
+		contentPane.add(btnEight);
+		btnEight.addActionListener(commonListener);
+		
+		JButton btnNine = new JButton("9");
+		btnNine.setBounds(166, 199, 49, 29);
+		contentPane.add(btnNine);
+		btnNine.addActionListener(commonListener);
+		
+		JButton btnMultiplication = new JButton("X");
+		btnMultiplication.setBounds(240, 199, 49, 29);
+		contentPane.add(btnMultiplication);
+		btnMultiplication.addActionListener(commonListener);
+		
+		JButton btnFour = new JButton("4");
+		btnFour.setBounds(20, 243, 49, 29);
+		contentPane.add(btnFour);
+		btnFour.addActionListener(commonListener);
+		
+		JButton btnFive = new JButton("5");
+		btnFive.setBounds(92, 243, 49, 29);
+		contentPane.add(btnFive);
+		btnFive.addActionListener(commonListener);
+		
+		JButton btnSix = new JButton("6");
+		btnSix.setBounds(166, 243, 49, 29);
+		contentPane.add(btnSix);
+		btnSix.addActionListener(commonListener);
+		
+		JButton btnSubtraction = new JButton("-");
+		btnSubtraction.setBounds(240, 243, 49, 29);
+		contentPane.add(btnSubtraction);
+		btnSubtraction.addActionListener(commonListener);
+		
+		JButton btnOne = new JButton("1");
+		btnOne.setBounds(20, 287, 49, 29);
+		contentPane.add(btnOne);
+		btnOne.addActionListener(commonListener);
+		
+		JButton btnTwo = new JButton("2");
+		btnTwo.setBounds(92, 287, 49, 29);
+		contentPane.add(btnTwo);
+		btnTwo.addActionListener(commonListener);
+		
+		JButton btnThree = new JButton("3");
+		btnThree.setBounds(166, 287, 49, 29);
+		contentPane.add(btnThree);
+		btnThree.addActionListener(commonListener);
+		
+		JButton btnAddition = new JButton("+");
+		btnAddition.setBounds(240, 287, 49, 29);
+		contentPane.add(btnAddition);
+		btnAddition.addActionListener(commonListener);
+		
+		JButton btnZero = new JButton("0");
+		btnZero.setBounds(92, 330, 49, 29);
+		contentPane.add(btnZero);
+		btnZero.addActionListener(commonListener);
+		
+		JButton btnDot = new JButton(".");
+		btnDot.setBounds(166, 330, 49, 29);
+		contentPane.add(btnDot);
+		btnDot.addActionListener(commonListener);
+		
+		JButton btnEqual = new JButton("=");
+		btnEqual.setBounds(240, 330, 49, 29);
+		contentPane.add(btnEqual);
+		btnEqual.addActionListener(commonListener);
+		
+		
+	}
+	
+	private static void processButtons(String text) {
+		switch(text) {
+			case "AC":
+				display.setText("");
+				break;
+			case "DEL":
+				String current = display.getText();
+				if(!current.isEmpty()) {
+					display.setText(current.substring(0 , current.length() - 1));
+				}
+				break;
+			case "%":
+				try {
+					double value = Double.parseDouble(display.getText());
+					double result = value / 100;
+					display.setText(String.valueOf(result));
+				}catch(Exception e) {
+					display.setText("Error");
+				}
+				break;
+			case "=":
+				Result();
+				break;
+			default:
+				display.setText(display.getText() + text);
+				break;
+		}
+	}
+	
+	private static void Result() {
+		String[] parts;
+		double number1;
+		double number2;
+		String input = display.getText();
+		double result = 0;
+		
+		try {
+			if(input.contains("+")){
+				parts = input.split("\\+");
+				number1 = Double.parseDouble(parts[0]);
+				number2 = Double.parseDouble(parts[1]);
+				
+				result = Calculator.addition(number1, number2);
+				display.setText(String.valueOf(result));
+			}
+			
+			if(input.contains("-")) {
+				parts = input.split("\\-");
+				number1 = Double.parseDouble(parts[0]);
+				number2 = Double.parseDouble(parts[1]);
+				
+				result = Calculator.subtraction(number1, number2);
+				display.setText(String.valueOf(result));
+			}
+			
+			if(input.contains("*")) {
+				parts = input.split("\\*");
+				number1 = Double.parseDouble(parts[0]);
+				number2 = Double.parseDouble(parts[1]);
+				
+				result = Calculator.multiplication(number1, number2);
+				display.setText(String.valueOf(result));
+			}
+			
+			if(input.contains("/")) {
+				parts = input.split("\\/");
+				number1 = Double.parseDouble(parts[0]);
+				number2 = Double.parseDouble(parts[1]);
+				
+				result = Calculator.division(number1, number2);
+				display.setText(String.valueOf(result));
+			}
+			
+		}finally {
+			System.out.println("Calculation completed");
+		}
+	}
+}
